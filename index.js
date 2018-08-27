972cfc331efbaa67842d2be0fcac3aacd093c67b
Nickolas Fuentes <nickolasfuentes@Mark-1.attlocal.net>
2018-08-27 00:49:56 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({Holidays: "Halloween, Thanksgiving, Christmass, New Years"});
 });
 
 const PORT = process.env.PORT || 5000;
